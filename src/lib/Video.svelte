89b1c03816cadc546401c3bb7c0ca4184552b8c5
Sabrina <59457991+syenko@users.noreply.github.com>
2023-06-19 16:20:17 -0700

    Add about section and video section

---
+++ src/lib/Video.svelte
@@ -0,0 +1,12 @@
+<div class="w-full flex flex-col items-center justify-center">
+	<div><h1>See us in action</h1></div>
+	<iframe
+		width="560"
+		height="315"
+		src="https://www.youtube-nocookie.com/embed/CuHgPlsjy2E"
+		title="YouTube video player"
+		frameborder="0"
+		allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+		allowfullscreen
+	/>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
 	import DATA from '$lib/data.js';
 	import Navbar from '$lib/Navbar.svelte';
 	import Header from '$lib/Header.svelte';
+	import About from '$lib/About.svelte';
 	import Carousel from '$lib/Carousel.svelte';
+	import Video from '$lib/Video.svelte';
 </script>
 
 <Navbar />
 <Header />
+<About />
 <Carousel images={DATA.IMAGE_DATA.IMAGES_LIST} />
+<Video />
