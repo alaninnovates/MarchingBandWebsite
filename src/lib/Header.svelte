9bfae8d94355aafa5ee78c673d217c45e298f9d0
Sabrina <59457991+syenko@users.noreply.github.com>
2023-06-20 23:24:46 -0700

    Adjust header text and image

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,11 +10,15 @@
 <div class="w-full h-[50rem] bg-cover bg-center bg-blend-darken bg-header">
 	<div class="w-full h-full bg-gradient-to-b from-black">
 		<div class=""><Navbar {links} /></div>
-		<div class="flex h-full w-full items-center justify-center pb-20">
+		<div class="flex h-full w-full items-center justify-center pb-20 px-12">
 			<div class="text-center p-10">
-				<h1 class="text-4xl text-white font-extrabold font-display">
-					Los Altos High School Marching Band
-				</h1>
+				<span class="block text-3xl text-white font-extrabold font-display italic pb-3">
+					Los Altos High School
+				</span>
+				<span class="block text-7xl text-white font-extrabold font-display">
+					Marching Band and Color Guard
+				</span>
+				<br />
 				<a href="#join" class="inline-block rounded-md bg-mblue text-white px-5 py-3 mt-3"
 					>Sign Up</a
 				>
