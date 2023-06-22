cf15bc04498773a57f2261d61af8f1cec8233224
Sabrina <59457991+syenko@users.noreply.github.com>
2023-06-22 14:05:44 -0700

    Add sign up underline

--- src/lib/JoinUs.svelte
+++ src/lib/JoinUs.svelte
@@ -4,7 +4,9 @@
 </script>
 
 <div class="flex flex-col justify-center items-center">
-	<div class="font-header text-4xl text-center mb-4">How do I sign up?</div>
+	<div class="font-header text-4xl text-center mb-4 underline underline-offset-2">
+		How do I sign up?
+	</div>
 	<div class="w-full max-w-4xl flex flex-col justify-center items-center px-10 text-xl">
 		<div class="">It’s easy! We will be hosting Band Camp for the 2023-2024 school year from</div>
 		<div class="font-bold mb-2">August 3rd - 5th.</div>
