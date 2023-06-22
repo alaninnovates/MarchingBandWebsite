7012bc3f40911198510b9a0b2820ffbd6e0347a2
Sabrina <59457991+syenko@users.noreply.github.com>
2023-06-22 14:30:10 -0700

    Remake "Why marching band" section

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -2,17 +2,23 @@
 	import Heading from '$lib/Heading.svelte';
 </script>
 
-<div class="w-full flex justify-center bg-mblue mt-10 pb-10 px-8 text-white">
-	<Heading>Why marching band?</Heading>
-	<div class="mx-8">
-		<div class="max-w-4xl">
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-				laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-				voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-				cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</p>
+<div class="w-full flex justify-center bg-mblue mt-10 text-white py-5">
+	<img
+		class="w-96 object-cover"
+		src="/images/whymarchingband.jpg"
+		alt="Group selfie of marching band members."
+	/>
+	<div class="mx-8 flex justify-center items-center">
+		<div class="max-w-2xl">
+			<div class="text-2xl font-header mb-3">Why marching band?</div>
+			<div>
+				The marching band community is a welcoming one, and anyone who joins soon becomes a part of
+				our diverse and supportive student network. Through band, students are able to bond with
+				each other via rehearsals, retreats, and our many events over the course of the season. They
+				not only develop instrumental skills, but also learn the importance of teamwork, as well as
+				what it takes to produce an entire field show. (Plus, you’ll get some PE credit* out of it!)
+			</div>
+			<div class="mt-3 text-sm">*Not available for freshmen</div>
 		</div>
 	</div>
 </div>
