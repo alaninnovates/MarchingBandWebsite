9f5c2dbf41a69552394e214a9cb93625bffac7d4
Sabrina <59457991+syenko@users.noreply.github.com>
2023-06-24 16:07:55 -0700

    Adjust heading to use new font

--- src/lib/Heading.svelte
+++ src/lib/Heading.svelte
@@ -2,6 +2,6 @@
 	export let id = '';
 </script>
 
-<div {id} class="font-bold text-center text-2xl mb-2 pt-10">
+<div {id} class="font-header text-center text-3xl mb-2 pt-10">
 	<span class="p-2"><slot /></span>
 </div>
