a76543b7b5442182ee98c179cc6734374e72bb02
Sabrina <59457991+syenko@users.noreply.github.com>
2023-06-24 22:47:14 -0700

    Adjust Gallery spacing

--- src/lib/Gallery.svelte
+++ src/lib/Gallery.svelte
@@ -9,8 +9,10 @@
 </script>
 
 <Heading>Marching band members travel to all sorts of exotic places!</Heading>
-<div class="flex flex-wrap justify-center items-center">
-	{#each photos as photo}
-		<Photo {...photo} />
-	{/each}
+<div class="flex justify-center w-full">
+	<div class="flex flex-wrap justify-center items-center max-w-[1000px]">
+		{#each photos as photo}
+			<Photo {...photo} />
+		{/each}
+	</div>
 </div>
