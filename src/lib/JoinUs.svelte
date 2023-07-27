571936829bbb0830c600e1ecb9f4b61382c28363
Sabrina <59457991+syenko@users.noreply.github.com>
2023-07-26 23:10:23 -0700

    Update mailing list link

--- src/lib/JoinUs.svelte
+++ src/lib/JoinUs.svelte
@@ -36,9 +36,30 @@
 		</div>
 		<div>Come visit to see if marching band is right for you!</div>
 
-		<div class="my-4">
-			For more updates,
-			<a href="mailto:{googleGroupEmail}" class="underline text-blue-500">join our mailing list!</a>
+		<div class="my-4 flex items-center">
+			<p>
+				For more updates, <a
+					href="https://docs.google.com/forms/d/e/1FAIpQLSdZBQR55lVr0-gf81fvLGNkAxfP7ftQ6cFh5LSI4qyr-VcwsA/viewform"
+					target="_blank"
+					class="underline text-blue-500">join our mailing list!</a
+				>
+			</p>
+			<a href="mailto:{googleGroupEmail}" class="ms-2 text-blue-500">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+					/>
+				</svg>
+			</a>
 		</div>
 	</div>
 </div>
